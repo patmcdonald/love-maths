879879a9151b78adcae410b0cb23252af087c2de
Pat McDonald <patmcdonald@me.com>
2022-01-18 21:07:16 +0000

    add operand code to function displayAdditionQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type")
-                alert(`You clicked ${gameType}`)
-
+                runGame(gameType)
             }
-                
         })
     }
+
+    runGame("addition")
 })
 
 /**
- * Main game loop is called when script is initially loaded
+ * Main game "loop" is called when script is initially loaded
  * and after user input has been processed 
  */
-function runGame() {
+function runGame(gameType) {
 
 // creates 2 random numbers 1 - 25
 let num1 = Math.floor(Math.random() * 24) + 1
 let num2 = Math.floor(Math.random() * 24) + 1
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2)
+} else {
+    alert(`Unknown game type: ${gameType}`)
+    throw `Unknown game type: ${gameType}. Aborting!`
+}
 
 }
 
@@ -47,7 +54,10 @@ function incrementWrongAnswer() {
 }
 
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1
+    document.getElementById(`operand2`).textContent = operand2
+    document.getElementById(`operator`).textContent = "+"
 
 }
 
