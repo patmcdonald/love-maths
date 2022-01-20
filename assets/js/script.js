70398e4b6fe9a2c2423142ef13ea51dc13d2148e
Pat McDonald <patmcdonald@me.com>
2022-01-20 12:09:05 +0000

    code incrementAnswer and incrementWrongAnswer tallys

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,9 +47,11 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0]
 
     if (isCorrect) {
-        alert("Hey! YOu got it right! :D")
+        alert("Hey! You got it right! :D")
+        incrementScore()
     } else {
         alert(`Ach...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer()
     }
 
     runGame(calculatedAnswer[1])
@@ -73,12 +75,25 @@ function calculateCorrectAnswer() {
     }
 
 }
-
+/**
+ * gets the current score from the DOM and increment it by 1
+ * incremented score is written back to the DOM
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").innerText = ++oldScore
 
+}
+/**
+ * gets the current tally from the DOM and increments it by 1
+ * incremented wrong answers are written back to the DOM
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText)
+    document.getElementById("incorrect").innerText = ++oldScore
+
 
 }
 
