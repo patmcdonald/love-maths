efac38e1e52d92bf81217fcf0fbd90d6828b87d9
Pat McDonald <patmcdonald@me.com>
2022-01-20 11:01:30 +0000

    checkAnswer function code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this. getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!")
+                checkAnswer()
             } else {
                 let gameType = this.getAttribute("data-type")
                 runGame(gameType)
@@ -36,9 +36,23 @@ if (gameType === "addition") {
 }
 
 }
-
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value)
+    let calculatedAnswer = calculateCorrectAnswer()
+    let isCorrect = userAnswer === calculatedAnswer[0]
+
+    if (isCorrect) {
+        alert("Hey! YOu got it right! :D")
+    } else {
+        alert(`Ach...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1])
 }
 
 /**
